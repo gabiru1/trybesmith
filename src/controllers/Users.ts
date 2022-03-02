987abf3681dc128f05df806d1ca9676a3c568f2f
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-03-02 17:40:14 -0300

    insert interface in user controller

--- src/controllers/Users.ts
+++ src/controllers/Users.ts
@@ -2,7 +2,11 @@ import { Request, Response } from 'express';
 import { User } from '../interfaces/UsersInterface';
 import Users from '../services/Users';
 
-const createUser = async (req: Request, res: Response) => {
+interface TOKEN {
+  token: string;
+}
+
+const createUser = async (req: Request, res: Response<TOKEN>) => {
   const { username, classe, level, password } = req.body as User;
 
   const user = await Users.createUser({ username, classe, level, password });
